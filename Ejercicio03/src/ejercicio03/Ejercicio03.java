c65c96d119921121f4e42dfda2284d37182e6c33
Fernando Armijos <fjarmijos1@utpl.edu.ec>
2022-10-30 11:08:39 -0500

    Ejercicios del taller03

--- Ejercicio03/src/ejercicio03/Ejercicio03.java
+++ Ejercicio03/src/ejercicio03/Ejercicio03.java
@@ -1,13 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ejercicio03;
 
 /**
  *
- * @author reroes
+ * @author fer33
  */
 public class Ejercicio03 {
 
@@ -15,16 +14,22 @@ public class Ejercicio03 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here 
         int var1 = 10;
         int var2 = 2;
         int var3 = 16;
-        int var4 =20;
-        int var5 =1;
-        int var6 =11;
-        //System.out.println(Math.pow(var1, var2)sqrt(var3) + var4 DIV (var5 + var6);
+        int var4 = 20;
+        int var5 = 1;
+        int var6 = 11;
+    
+        // System.out.println(Math.pow(var1, var2)sqrt(var3) + var4 DIV(var5 +var6);
         double valor = Math.pow(var1, var2) + Math.sqrt(var3) + var4 / (var5 + var6);
         System.out.println(valor);
+    
+        
+    
+    
+    
     }
     
 }
---
+++ Ejercicio10/src/ejercicio10/Ejercicio10.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio10;
+
+/**
+ *
+ * @author fer33
+ */
+public class Ejercicio10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        int var1 = 25;
+        int var2 = 10;
+        int var3 = 100;
+        int var4 = 10;
+        int var5 = 5;
+        int var6 = 2;
+        // System.out.println(Math.sqrt(var1)* 10>= var3) && (true) || (false) || (var4 / var5>= var6);
+        boolean valor =(Math.sqrt(var1)* var2>= var3)&& (true) || (false) || (var4 /var5>= var6);
+        System.out.println(valor);
+        
+    
+    }
+    
+}
